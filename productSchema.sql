637b9c3722818f1c1aa9fb99efe21b5921d64c6f
Jesse312 <jesse.fuu@gmail.com>
2022-07-27 20:48:04 -0700

    Schema and ETL applied.

--- productSchema.sql
+++ productSchema.sql
@@ -1,61 +1,71 @@
 DROP DATABASE IF EXISTS productSection;
 CREATE DATABASE productSection;
+\c [productsection];
+-- Create database prior to running code below
 
 DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
   id int NOT NULL PRIMARY KEY,
-  product_name VARCHAR(255),
-  slogan VARCHAR(255),
-  product_description VARCHAR(255),
-  category VARCHAR(255),
-  default_price int,
-  created_at DATE DEFAULT NULL,
-  updated_at DATE DEFAULT NULL
-  -- below to be optimized
-  -- features text[]
+  name VARCHAR(255) NOT NULL,
+  slogan VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  default_price int NOT NULL
   );
 
 DROP TABLE IF EXISTS styles;
 CREATE TABLE styles (
-  style_id int NOT NULL PRIMARY KEY,
-  color VARCHAR(255),
-  style_name VARCHAR(500),
-  original_price VARCHAR(20),
+  id int NOT NULL PRIMARY KEY,
+  productId int NOT NULL,
+  name VARCHAR(500) NOT NULL,
   sale_price VARCHAR(20),
-  default_style BOOLEAN,
-  id int,
-  FOREIGN KEY (id) REFERENCES products(id),
-  -- below to be optimized
-  skus text[],
-  photos text[]
+  original_price VARCHAR(20) NOT NULL,
+  default_style BOOLEAN NOT NULL,
+  main_id int,
+  FOREIGN KEY (main_id) REFERENCES products(id)
+);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE photos (
+id int NOT NULL PRIMARY KEY,
+styleId int NOT NULL,
+url TEXT,
+thumbnail_url TEXT,
+main_id int,
+FOREIGN KEY (main_id) REFERENCES products(id)
 );
 
 DROP TABLE IF EXISTS skus;
 CREATE TABLE skus (
-
-)
+  sku_id int NOT NULL PRIMARY KEY,
+  styleID int NOT NULL,
+  size VARCHAR(255) NOT NULL,
+  quantity int NOT NULL,
+  main_id int,
+  FOREIGN KEY (main_id) REFERENCES products(id)
+);
 
 DROP TABLE IF EXISTS related;
 CREATE TABLE related (
-  product_id int NOT NULL PRIMARY KEY,
-  related integer[],
-  id int,
-  FOREIGN KEY (id) REFERENCES products(id)
+  id int NOT NULL PRIMARY KEY,
+  current_product_id int NOT NULL,
+  related_product_id int NOT NULL,
+  main_id int,
+  FOREIGN KEY (main_id) REFERENCES products(id)
 );
 
 DROP TABLE IF EXISTS features;
 CREATE TABLE features (
-  product_id int NOT NULL PRIMARY KEY,
-  feature_feature VARCHAR(255),
-  feature_value VARCHAR(255),
-  id int,
-  FOREIGN KEY (id) REFERENCES products(id)
+  id int NOT NULL PRIMARY KEY,
+  product_id int,
+  feature VARCHAR(1000) NOT NULL,
+  value VARCHAR(1000) NOT NULL,
+  main_id int,
+  FOREIGN KEY (main_id) REFERENCES products(id)
 );
 
-//run command to connect to database
 
 /*  Execute this file from the command line by typing:
 Code doesn't actually create anything at the moment - potential permissions issue
  *    psql -U postgres < /Users/hunny/Atelier-Backend/productSchema.sql
-  *   psql postgres -h 127.0.0.1 -d qa -f/Users/hunny/Atelier-Backend/productSchema.sql
  *  to create the database and the tables.*/

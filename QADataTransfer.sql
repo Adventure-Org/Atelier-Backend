6954ff5de98cb0a9f1d8445015c85d040952aa9d
rochun <royce.h.chun@gmail.com>
2022-07-27 20:57:07 -0700

    combine files into one sql file

--- QADataTransfer.sql
+++
@@ -1,5 +0,0 @@
-\COPY questions(id , product_id, body, date_written, asker_name, asker_email, reported, helpful) FROM '/Users/roycechun/Desktop/RFP2205/Atelier-Backend/data/questions.csv' CSV HEADER;
-
-\COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) FROM '/Users/roycechun/Desktop/RFP2205/Atelier-Backend/data/answers.csv' CSV HEADER;
-
-\COPY answer_photos(id, answer_id, url) FROM '/Users/roycechun/Desktop/RFP2205/Atelier-Backend/data/answers_photos.csv' CSV HEADER;

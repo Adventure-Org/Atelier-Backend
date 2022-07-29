db41048eae337b414fe02ddc4873cdcb7ebc32e7
Andy Chow <andychow94@gmail.com>
2022-07-29 16:00:45 -0700

    Added GET /reviews/ path to return reviews object

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -10,27 +10,43 @@ const pool = new Pool({
 
 exports.getReviews = (req, res) => {
   // console.log('Req:', req);
-  pool.query('SELECT * FROM reviews LIMIT 1')
+  //'SELECT * FROM reviews LIMIT 1'
+  console.log('req.query', req.query);
+  console.log('Check count:', req.query.count || 5);
+  const product_id = req.query.product_id;
+  const count = req.query.count || 5;
+  const page = req.query.page || 0;
+  let resultObj = {
+    product: product_id,
+    page: page,
+    count: count,
+    results: [],
+  };
+  const queryString = 'SELECT reviews.*, (SELECT json_agg(reviews_photos.*) FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id) as photos FROM reviews WHERE reviews.product_id = ' + product_id;
+  // pool.query('SELECT reviews.*, (SELECT json_agg(reviews_photos.*) FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id) as photos FROM reviews WHERE reviews.product_id = 1')
+  // pool.query('SELECT * FROM reviews LEFT JOIN reviews_photos ON reviews.review_id = reviews_photos.review_id WHERE reviews.product_id = 5')
+  // pool.query('SELECT * FROM reviews r LEFT JOIN reviews_photos p ON r.review_id = p.review_id WHERE r.product_id = 5')
+  pool.query(queryString)
     .then((result) => {
-      console.log('Result from Reviews: ', result.rows);
-    })
-    .then(() => {
-      return pool.query('SELECT * FROM reviews_photos LIMIT 1')
-    })
-    .then((result) => {
-      console.log('Result from reviews_photos: ', result.rows);
-    })
-    .then(() => {
-      return pool.query('SELECT * FROM characteristics LIMIT 1')
-    })
-    .then((result) => {
-      console.log('Result from characteristics: ', result.rows);
-    })
-    .then(() => {
-      return pool.query('SELECT * FROM characteristics_reviews LIMIT 1')
-    })
-    .then((result) => {
-      console.log('Result from characteristics_reviews: ', result.rows);
+      // console.log('Result from Reviews: ', result.rows);
+      let records = result.rows;
+      for (let i = 0; i < records.length; i += 1) {
+        let container = {
+          review_id: records[i].review_id,
+          rating: records[i].rating,
+          summary: records[i].summary,
+          recommend: records[i].recommended,
+          response: records[i].response,
+          body: records[i].body,
+          date: records[i].date,
+          review_name: records[i].reviewer_name,
+          helpfulness: records[i].helpfulness,
+          photos: records[i].photos,
+        };
+        resultObj.results.push(container);
+      }
+      // console.log('Final Object:', resultObj);
+      res.status(201).send(resultObj);
     })
     .catch((err) => {
       console.log('Error in getReviews:', err);

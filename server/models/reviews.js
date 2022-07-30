649a551eb5287dad1a6067d21221ce2201db9da3
Andy Chow <andychow94@gmail.com>
2022-07-30 02:16:55 -0700

    Added GET /reviews/metadata and returns correct data

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -11,8 +11,8 @@ const pool = new Pool({
 exports.getReviews = (req, res) => {
   // console.log('Req:', req);
   //'SELECT * FROM reviews LIMIT 1'
-  console.log('req.query', req.query);
-  console.log('Check count:', req.query.count || 5);
+  // console.log('req.query', req.query);
+  // console.log('Check count:', req.query.count || 5);
   const product_id = req.query.product_id;
   const count = req.query.count || 5;
   const page = req.query.page || 0;
@@ -22,7 +22,7 @@ exports.getReviews = (req, res) => {
     count: count,
     results: [],
   };
-  const queryString = 'SELECT reviews.*, (SELECT json_agg(reviews_photos.*) FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id) as photos FROM reviews WHERE reviews.product_id = ' + product_id;
+  const queryString = 'SELECT reviews.*, (SELECT json_agg(reviews_photos.*) FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id) AS photos FROM reviews WHERE reviews.product_id = ' + product_id;
   // pool.query('SELECT reviews.*, (SELECT json_agg(reviews_photos.*) FROM reviews_photos WHERE reviews.review_id = reviews_photos.review_id) as photos FROM reviews WHERE reviews.product_id = 1')
   // pool.query('SELECT * FROM reviews LEFT JOIN reviews_photos ON reviews.review_id = reviews_photos.review_id WHERE reviews.product_id = 5')
   // pool.query('SELECT * FROM reviews r LEFT JOIN reviews_photos p ON r.review_id = p.review_id WHERE r.product_id = 5')
@@ -54,11 +54,74 @@ exports.getReviews = (req, res) => {
 };
 
 exports.getMetadata = (req, res) => {
-  // pool.query('SELECT * FROM reviews_photos LIMIT 5')
-  //   .then((result) => {
-  //     console.log('Result in getReviewsPhotos:', result.rows);
-  //   })
-  //   .catch((err) => {
-  //     console.log('Error in getReviewsPhotos:', err);
-  //   })
+  const product_id = req.query.product_id;
+  let resultObj = {
+    product: product_id,
+    ratings: {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    },
+    recommended: {
+      true: 0,
+      false: 0,
+    },
+    characteristics: {},
+  };
+  // const queryString = 'SELECT r.rating, r.recommended FROM reviews r INNER JOIN characteristics_reviews cr on r.review_id = cr.review_id INNER JOIN characteristics c on cr.characteristic_id = c.characteristics_id WHERE r.product_id = '+product_id;
+  //'SELECT r.rating, r.recommended FROM reviews r INNER JOIN characteristics_reviews cr on r.review_id = cr.review_id INNER JOIN characteristics c on cr.characteristic_id = c.characteristics_id WHERE r.product_id = '+product_id;
+  // const queryString = '(SELECT json_agg(cr.characteristics_reviews_id) FROM characteristics_reviews cr INNER JOIN characteristics c INNER JOIN ON cr.characteristic_id = c.characteristics_id) AS id'
+
+  //Below are good to go
+  // const queryString = 'SELECT json_agg(c.name) as characteristics FROM characteristics c WHERE c.product_id = '+product_id;
+  // const queryString = 'SELECT r.rating, r.recommended FROM reviews r where r.product_id = '+ product_id;
+
+  const queryFirstString = 'SELECT r.rating, r.recommended, (SELECT json_agg(c.name) as characteristics FROM characteristics c WHERE c.product_id = '+product_id+') FROM reviews r where r.product_id = '+ product_id;
+  const querySecondString = 'SELECT cr.characteristic_id, cr.value, c.name FROM characteristics_reviews cr INNER JOIN characteristics c on c.characteristics_id = cr.characteristic_id WHERE c.product_id = '+product_id;
+
+  // const queryString = 'SELECT json_agg(cr) as characteristics FROM characteristics_reviews cr LEFT JOIN characteristics c on c.characteristics_id = cr.characteristic_id WHERE c.product_id = '+product_id;
+  pool.query(queryFirstString)
+    .then((result) => {
+      // console.log('Result in getMetadata:', result.rows);
+      let trueCount = 0;
+      let falseCount = 0;
+      for (let i = 0; i < result.rows.length; i += 1) {
+        resultObj.ratings[result.rows[i].rating] += 1;
+        resultObj.recommended[result.rows[i].recommended] += 1;
+      }
+      // console.log(resultObj);
+      return pool.query(querySecondString);
+    })
+    .then((result) => {
+      // console.log('Second Query:', result.rows);
+      let uniqueCount = 0;
+      let totalCount = 0;
+      for (let i = 0; i < result.rows.length; i += 1) {
+        if(resultObj.characteristics[result.rows[i].name] === undefined) {
+          resultObj.characteristics[result.rows[i].name] = {
+            id: result.rows[i].characteristic_id,
+            value: result.rows[i].value,
+          };
+          // Keep track of how many different characteristics
+          uniqueCount += 1;
+          totalCount += 1;
+        } else {
+          resultObj.characteristics[result.rows[i].name].value += result.rows[i].value;
+          // Keep track of how many ratings we added
+          totalCount += 1;
+        }
+      }
+      totalCount /= uniqueCount;
+      for (var key in resultObj.characteristics) {
+        // Calculate the average rating - assuming each characteristic has same amount of ratings
+        resultObj.characteristics[key].value /= totalCount;
+      }
+      // console.log('Second result obj:', resultObj);
+      res.status(201).send(resultObj);
+    })
+    .catch((err) => {
+      console.log('Error in getMetadata:', err);
+    })
 }
--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const db = require('./models/reviews.js');
 
 router.get('/reviews', db.getReviews);
-router.get('/reviewsPhotos', db.getMetadata);
+router.get('/reviews/meta', db.getMetadata);
 
 module.exports = router;
